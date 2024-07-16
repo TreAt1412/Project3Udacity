f9500f92db95548e1665fd74a60f3d5610d16160
Your Name <TreAt1412>
2024-07-16 22:49:31 +0700

    change selenium file

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 import logging as log
-
+from selenium.webdriver.common.by import By
 # Start the browser and login with standard_user
 def login (user, password):
     log.info('Starting the browser...')
@@ -13,13 +13,13 @@ def login (user, password):
     
     log.info('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
-    driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
-    driver.find_element_by_css_selector("input[id='password']").send_keys(password)
-    driver.find_element_by_css_selector("input[id='login-button']").click()
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+    driver.find_element(By.CSS_SELECTOR, "input[id='login-button']").click()
 
     path_wrapper = "div[id='page_wrapper'] > div[id='contents_wrapper']"
     path_product = path_wrapper + " > div[id='header_container'] > div[class='header_secondary_container'] > span[class='title']"
-    product_logo = driver.find_element_by_css_selector(path_product).text
+    product_logo = driver.find_element(By.CSS_SELECTOR, path_product).text
 
     assert "Products" in product_logo
     log.info("Success login")
@@ -27,16 +27,16 @@ def login (user, password):
     # add item to cart
     log.info("Starting add items to cart")
     path_items = path_wrapper + " > div[id='inventory_container'] > div[id='inventory_container'] > div[class='inventory_list'] > div[class='inventory_item'] " 
-    product_items = driver.find_elements_by_css_selector(path_items)
+    product_items = driver.find_element(By.CSS_SELECTOR, path_items)
     assert len(product_items) == 6
     log.info("Successfully found 6 product items.")    
 
     for i in range(6):
         path_to_item = path_items + " > div[class='inventory_item_description'] > div[class='inventory_item_label'] > a[id='item_" + str(i) + "_title_link']"
-        driver.find_elements_by_css_selector(path_to_item).find_element_by_xpath('..//..//..//div[@class="pricebar"]//button[@class="btn_primary btn_inventory"]').click()
+        driver.find_element(By.CSS_SELECTOR, path_to_item).find_element_by_xpath('..//..//..//div[@class="pricebar"]//button[@class="btn_primary btn_inventory"]').click()
 
     path_shopping_cart_badge = "div[id='page_wrapper'] > div[id='contents_wrapper'] > div[id='header_container'] > div[id='shopping_cart_container'] > a[class='shopping_cart_link'] > span[class='shopping_cart_badge']"
-    total_items_text = driver.find_element_by_css_selector(path_shopping_cart_badge).text
+    total_items_text = driver.find_element(By.CSS_SELECTOR, path_shopping_cart_badge).text
     assert '6' == total_items_text
     log.info("Success add 6 items to cart")
     
@@ -48,10 +48,10 @@ def login (user, password):
 
     for i in range(6):
         path_to_item = path_items + " > div[class='inventory_item_description'] > div[class='inventory_item_label'] > a[id='item_" + str(i) + "_title_link']"
-        driver.find_elements_by_css_selector(path_to_item).find_element_by_xpath('..//..//..//div[@class="pricebar"]//button[@class="btn_secondary btn_inventory"]').click()
+        driver.find_element(By.CSS_SELECTOR,path_to_item).find_element_by_xpath('..//..//..//div[@class="pricebar"]//button[@class="btn_secondary btn_inventory"]').click()
 
     path_shopping_cart_badge = "div[id='page_wrapper'] > div[id='contents_wrapper'] > div[id='header_container'] > div[id='shopping_cart_container'] > a[class='shopping_cart_link'] > span[class='shopping_cart_badge']"
-    total_items_text = driver.find_element_by_css_selector(path_shopping_cart_badge).text
+    total_items_text = driver.find_element(By.CSS_SELECTOR, path_shopping_cart_badge).text
     assert '0' == total_items_text
     log.info("Success remove all items from cart")
 
