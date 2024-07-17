958ebe40555dc91b58766ca0628951a9e7591844
Anh dv <37327111+TreAt1412@users.noreply.github.com>
2024-07-17 14:24:33 +0700

    Update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -23,36 +23,33 @@ def login (user, password):
 
     assert "Products" in product_logo
     log.info("Success login")
-
     # add item to cart
     log.info("Starting add items to cart")
-    path_items = path_wrapper + " > div[id='inventory_container'] > div[id='inventory_container'] > div[class='inventory_list'] > div[class='inventory_item'] " 
-    product_items = driver.find_element(By.CSS_SELECTOR, path_items)
-    assert len(product_items) == 6
-    log.info("Successfully found 6 product items.")    
 
     for i in range(6):
-        path_to_item = path_items + " > div[class='inventory_item_description'] > div[class='inventory_item_label'] > a[id='item_" + str(i) + "_title_link']"
-        driver.find_element(By.CSS_SELECTOR, path_to_item).find_element_by_xpath('..//..//..//div[@class="pricebar"]//button[@class="btn_primary btn_inventory"]').click()
+        path_to_item = "a[id='item_" + str(i) + "_title_link']"
+        item = driver.find_element(By.CSS_SELECTOR, path_to_item)
+        item.find_element(By.XPATH, '../../div[@class="pricebar"]/button').click()
 
-    path_shopping_cart_badge = "div[id='page_wrapper'] > div[id='contents_wrapper'] > div[id='header_container'] > div[id='shopping_cart_container'] > a[class='shopping_cart_link'] > span[class='shopping_cart_badge']"
+    path_shopping_cart_badge = "div[id='page_wrapper'] > div[id='contents_wrapper'] > div[id='header_container'] > div[class='primary_header'] > div[id='shopping_cart_container'] > a[class='shopping_cart_link'] > span[class='shopping_cart_badge']"
     total_items_text = driver.find_element(By.CSS_SELECTOR, path_shopping_cart_badge).text
     assert '6' == total_items_text
     log.info("Success add 6 items to cart")
     
-    # remoe item from cart
+    # remove item from cart
 
     log.info("Starting remove all items from cart")
     path_items = path_wrapper + " > div[id='inventory_container'] > div[id='inventory_container'] > div[class='inventory_list'] > div[class='inventory_item'] " 
     
 
     for i in range(6):
-        path_to_item = path_items + " > div[class='inventory_item_description'] > div[class='inventory_item_label'] > a[id='item_" + str(i) + "_title_link']"
-        driver.find_element(By.CSS_SELECTOR,path_to_item).find_element_by_xpath('..//..//..//div[@class="pricebar"]//button[@class="btn_secondary btn_inventory"]').click()
+        path_to_item = "a[id='item_" + str(i) + "_title_link']"
+        item = driver.find_element(By.CSS_SELECTOR, path_to_item)
+        item.find_element(By.XPATH, '../../div[@class="pricebar"]/button').click()
 
-    path_shopping_cart_badge = "div[id='page_wrapper'] > div[id='contents_wrapper'] > div[id='header_container'] > div[id='shopping_cart_container'] > a[class='shopping_cart_link'] > span[class='shopping_cart_badge']"
+    path_shopping_cart_badge = "div[id='page_wrapper'] > div[id='contents_wrapper'] > div[id='header_container'] > div[class='primary_header'] > div[id='shopping_cart_container'] > a[class='shopping_cart_link']"
     total_items_text = driver.find_element(By.CSS_SELECTOR, path_shopping_cart_badge).text
-    assert '0' == total_items_text
+    assert '' == total_items_text
     log.info("Success remove all items from cart")
 
 
