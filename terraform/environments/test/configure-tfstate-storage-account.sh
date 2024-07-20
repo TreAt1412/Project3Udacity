2c8a2e0e4eb755633945236cc4e639feca0b8d89
Your Name <TreAt1412>
2024-07-20 08:10:05 +0700

    change public key

--- terraform/environments/test/configure-tfstate-storage-account.sh
+++ terraform/environments/test/configure-tfstate-storage-account.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 RESOURCE_GROUP_NAME="Azuredevops"
-STORAGE_ACCOUNT_NAME="tstateanhdv29"
+STORAGE_ACCOUNT_NAME="tfstateanhdv29"
 CONTAINER_NAME="tfstate"
 
 # This command is not needed in the Udacity provided Azure account. 
