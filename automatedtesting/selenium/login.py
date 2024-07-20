d61b595801e1ee75caf5f62bc362074d6b8d2a87
Your Name <TreAt1412>
2024-07-20 15:13:41 +0700

    change terraform

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -5,13 +5,13 @@ import logging as log
 from selenium.webdriver.common.by import By
 # Start the browser and login with standard_user
 def login (user, password):
-    log.info('Starting the browser...')
+    print('Starting the browser...')
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
     options.add_argument("--headless") 
     driver = webdriver.Chrome(options=options)
     
-    log.info('Browser started successfully. Navigating to the demo page to login.')
+    print('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
     driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
     driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
@@ -22,9 +22,9 @@ def login (user, password):
     product_logo = driver.find_element(By.CSS_SELECTOR, path_product).text
 
     assert "Products" in product_logo
-    log.info("Success login")
+    print("Success login")
     # add item to cart
-    log.info("Starting add items to cart")
+    print("Starting add items to cart")
 
     for i in range(6):
         path_to_item = "a[id='item_" + str(i) + "_title_link']"
@@ -34,11 +34,11 @@ def login (user, password):
     path_shopping_cart_badge = "div[id='page_wrapper'] > div[id='contents_wrapper'] > div[id='header_container'] > div[class='primary_header'] > div[id='shopping_cart_container'] > a[class='shopping_cart_link'] > span[class='shopping_cart_badge']"
     total_items_text = driver.find_element(By.CSS_SELECTOR, path_shopping_cart_badge).text
     assert '6' == total_items_text
-    log.info("Success add 6 items to cart")
+    print("Success add 6 items to cart")
     
     # remove item from cart
 
-    log.info("Starting remove all items from cart")
+    print("Starting remove all items from cart")
     path_items = path_wrapper + " > div[id='inventory_container'] > div[id='inventory_container'] > div[class='inventory_list'] > div[class='inventory_item'] " 
     
 
@@ -50,7 +50,7 @@ def login (user, password):
     path_shopping_cart_badge = "div[id='page_wrapper'] > div[id='contents_wrapper'] > div[id='header_container'] > div[class='primary_header'] > div[id='shopping_cart_container'] > a[class='shopping_cart_link']"
     total_items_text = driver.find_element(By.CSS_SELECTOR, path_shopping_cart_badge).text
     assert '' == total_items_text
-    log.info("Success remove all items from cart")
+    print("Success remove all items from cart")
 
 
 login('standard_user', 'secret_sauce')
