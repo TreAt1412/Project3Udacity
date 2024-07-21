11b17a17acf1a84540eb9910287afb9a87844f86
Your Name <TreAt1412>
2024-07-21 10:05:39 +0700

    change terraform

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -9,7 +9,7 @@ resource "azurerm_app_service_plan" "test" {
 }
 
 resource "azurerm_linux_web_app" "test" {
-  name                = "webapp-anhdv2"
+  name                = "webapp-anhdv29"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   service_plan_id     = azurerm_app_service_plan.test.id
