52db6b45a0f045aa44f8ceed8a365fd14d24e034
Your Name <TreAt1412>
2024-07-21 13:49:33 +0700

    change terraform

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -19,6 +19,19 @@ def login (user, password):
     options.add_argument("--headless") 
     options.add_argument("--no-sandbox") 
     options.add_argument("--disable-dev-shm-usage") 
+    options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) 
+    options.add_argument("--no-sandbox") 
+    options.add_argument("--disable-setuid-sandbox") 
+
+    options.add_argument("--remote-debugging-port=9222")  # this
+
+    options.add_argument("--disable-dev-shm-using") 
+    options.add_argument("--disable-extensions") 
+    options.add_argument("--disable-gpu") 
+    options.add_argument("start-maximized") 
+    options.add_argument("disable-infobars")
+    options.add_argument(r"user-data-dir=.\cookies\\test") 
+
     service_obj = Service("/usr/bin/chromedriver")
     driver = webdriver.Chrome(service= service_obj, options=options)
     
