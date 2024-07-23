9e89bddb50e019d1bd1a2e5b44852c07ba756036
Your Name <TreAt1412>
2024-07-23 20:11:54 +0700

    change terraform

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -14,7 +14,7 @@ def timestamp():
 
 
 def login (user, password):
-    print( timestamp() + 'Starting the browser...')
+    # print( timestamp() + 'Starting the browser...')
     log.info( timestamp() + 'Starting the browser...')
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
@@ -25,7 +25,7 @@ def login (user, password):
     service_obj = Service(ChromeDriverManager(driver_version="126.0.6478.182").install())
     driver = webdriver.Chrome(service= service_obj, options=options)
     
-    print( timestamp() + 'Browser started successfully. Navigating to the demo page to login.')
+    # print( timestamp() + 'Browser started successfully. Navigating to the demo page to login.')
     log.info( timestamp() + 'Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
     driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
@@ -37,10 +37,10 @@ def login (user, password):
     product_logo = driver.find_element(By.CSS_SELECTOR, path_product).text
 
     assert "Products" in product_logo
-    print( timestamp() + "Success login")
+    # print( timestamp() + "Success login")
     log.info( timestamp() + "Success login")
     # add item to cart
-    print( timestamp() + "Starting add items to cart")
+    # print( timestamp() + "Starting add items to cart")
     log.info( timestamp() + "Starting add items to cart")
 
     for i in range(6):
@@ -51,12 +51,12 @@ def login (user, password):
     path_shopping_cart_badge = "div[id='page_wrapper'] > div[id='contents_wrapper'] > div[id='header_container'] > div[class='primary_header'] > div[id='shopping_cart_container'] > a[class='shopping_cart_link'] > span[class='shopping_cart_badge']"
     total_items_text = driver.find_element(By.CSS_SELECTOR, path_shopping_cart_badge).text
     assert '6' == total_items_text
-    print( timestamp() + "Success add 6 items to cart")
+    # print( timestamp() + "Success add 6 items to cart")
     log.info( timestamp() + "Success add 6 items to cart")
     
     # remove item from cart
 
-    print( timestamp() + "Starting remove all items from cart")
+    # print( timestamp() + "Starting remove all items from cart")
     log.info( timestamp() + "Starting remove all items from cart")
     path_items = path_wrapper + " > div[id='inventory_container'] > div[id='inventory_container'] > div[class='inventory_list'] > div[class='inventory_item'] " 
     
@@ -69,8 +69,8 @@ def login (user, password):
     path_shopping_cart_badge = "div[id='page_wrapper'] > div[id='contents_wrapper'] > div[id='header_container'] > div[class='primary_header'] > div[id='shopping_cart_container'] > a[class='shopping_cart_link']"
     total_items_text = driver.find_element(By.CSS_SELECTOR, path_shopping_cart_badge).text
     assert '' == total_items_text
-    print( timestamp() + "Success remove all items from cart")
+    # print( timestamp() + "Success remove all items from cart")
     log.info( timestamp() + "Success remove all items from cart")
 
-
+log.basicConfig(level=log.INFO)
 login('standard_user', 'secret_sauce')
